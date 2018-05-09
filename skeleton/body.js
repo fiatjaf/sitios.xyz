b41e119fcfd967028c0c5be75081b22a2c7ae32f
fiatjaf <fiatjaf@gmail.com>
2018-05-09 12:21:47 +0000

    upgrade to sitio@3.0.0

--- skeleton/body.js
+++ skeleton/body.js
@@ -1,7 +1,19 @@
 const h = require('react-hyperscript')
+const Helmet = require('react-safety-helmet').default
 
 module.exports = props => {
   return [
+    h(Helmet, {key: 'helmet'}, [
+      h('meta', {charset: 'utf-8'}),
+      h('meta', {httpEquiv: 'x-ua-compatible', content: 'ie: edge'}),
+      h('meta', {name: 'description', content: 'Pure-CSS themes for standard HTML formats.'}),
+      h('meta', {name: 'viewport', content: 'width=device-width, height=device-height, initial-scale=1.0, user-scalable=yes'}),
+      h('title', props.global.name),
+      h('link', {href: props.global.favicon, rel: 'shortcut icon'})
+    ].concat(
+      (props.global.includes || []).filter(isCSS)
+        .map(href => h('link', {href, rel: 'stylesheet'}))
+    )),
     h('header', {key: 'header', role: 'banner'}, [
       props.global.header ? h('img', {src: props.global.header}) : '',
       h('h1', [
@@ -14,7 +26,7 @@ module.exports = props => {
       })
     ]),
     h('nav', {key: 'nav'}, [
-      h('ul', props.global.nav.map(ni =>
+      h('ul', (props.global.nav || []).map(ni =>
         h('li', {key: ni.url}, [
           h('a', {href: ni.url}, ni.txt)
         ])
@@ -31,7 +43,7 @@ module.exports = props => {
       dangerouslySetInnerHTML: {__html: props.global.footer}
     })
   ].concat(
-    props.global.includes.filter(isJS)
+    (props.global.includes || []).filter(isJS)
       .map(src => h('script', {src, key: src}))
   ).concat(
     props.global.justhtml ? [] : h('script', {key: 'bundle', src: '/bundle.js'})
@@ -41,4 +53,9 @@ module.exports = props => {
 function isJS (url) {
   return url.split('?')[0]
     .slice(-3) === '.js'
+}
+
+function isCSS (url) {
+  return url.split('?')[0]
+    .slice(-4) === '.css'
 }
